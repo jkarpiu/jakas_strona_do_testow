3a932d2612cdec1f53df1d9561c598704c609a69
Łukasz Kliber <lucxsdesing14@gmail.com>
2020-11-19 19:51:36 +0100

    DZIAŁA JEDNA PODSTRONA W KONCU

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -6,20 +6,27 @@
     <form action="/dodaj_pytanie" method="POST">
         @csrf
         <h1 class="header1">Dodaj pytanie</h1>
-        <input class="form-control" placeholder="Pytanie" type="text" name="pytanie">
-        <select class="form-control" name="dzial">
+        <input class="form-control question-input" placeholder="Pytanie" type="text" name="pytanie">
+        <select class="form-control question-input" name="dzial">
             @foreach ($dzialy as $dzial)
                 <option value="{{ $dzial->id }}"> {{ $dzial->nazwa }} </option>
             @endforeach
         </Select>
         <br>
+        <h1 class="header2">Dodaj odpowiedzi</h1>
         @foreach (['a1', 'a2', 'a3', 'a4'] as $item)
-            <p>Poprawna: <input type="radio" name="valid" value="{{ $item }}">
-            <input placeholder="Odpowiedź" class="form-control" type="text" name="{{ $item }}" id=" {{ $item }} "></p>
-            <br>
+        <div id="head-answer-all">
+            <div id="answer-all">
+                <p class="check-valid">Poprawna: <input type="radio" name="valid" value="{{ $item }}"></p>
+                    <input class="form-control answer-input" placeholder="Odpowiedź"
+                    type="text" name="{{ $item }}" id=" {{ $item }} ">
+            </div>
+        </div> 
         @endforeach
-        <button type="submit" class="btn btn-primary">
-            Wyślij
-        </button>
+        <center>
+            <button type="submit" class="btn btn-primary">
+                Wyślij
+            </button>
+        </center>
     </form>
 @endsection
