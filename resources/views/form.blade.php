15761388518c19eb801392b580d2d1e7f9605343
Łukasz Kliber <lucxsdesing14@gmail.com>
2020-11-19 18:29:18 +0100

    teraz nie zniknie okej

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -5,18 +5,19 @@
 </div>
     <form action="/dodaj_pytanie" method="POST">
         @csrf
+        <h1 class="header1">Dodaj pytanie</h1>
         <input class="form-control" placeholder="Pytanie" type="text" name="pytanie">
+        <select class="form-control" name="dzial">
+            @foreach ($dzialy as $dzial)
+                <option value="{{ $dzial->id }}"> {{ $dzial->nazwa }} </option>
+            @endforeach
+        </Select>
         <br>
         @foreach (['a1', 'a2', 'a3', 'a4'] as $item)
             <p>Poprawna: <input type="radio" name="valid" value="{{ $item }}">
             <input placeholder="Odpowiedź" class="form-control" type="text" name="{{ $item }}" id=" {{ $item }} "></p>
             <br>
         @endforeach
-        <select class="form-control" name="dzial">
-            @foreach ($dzialy as $dzial)
-                <option value="{{ $dzial->id }}"> {{ $dzial->nazwa }} </option>
-            @endforeach
-        </Select>
         <button type="submit" class="btn btn-primary">
             Wyślij
         </button>
