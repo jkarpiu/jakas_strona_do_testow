063503bd89c3d97fcf481800c550c5f9ebe77d4f
Jakub Karp <karptutaj@gmail.com>
2020-11-28 15:43:45 +0100

    test 3

--- public/pull.php
+++ public/pull.php
@@ -3,5 +3,5 @@
 if ( $_POST['payload'] ) {
     shell_exec( "cd /var/www/jakas_strona_do_testow && sudo git reset --hard origin/master && sudo git pull" );
 }
-// git nowy test
+// git nowy nowy test
 ?>
