8ad610e785e2283519a214dd031461e4197341e0
Jakub Karp <karptutaj@gmail.com>
2020-11-29 16:42:27 +0100

    fixed

--- public/js/app.js
+++ public/js/app.js
@@ -2859,9 +2859,7 @@ __webpack_require__.r(__webpack_exports__);
       wyniki: []
     };
   },
-  created: function created() {
-    this.getWyniki();
-  }
+  created: function created() {}
 });
 
 /***/ }),
