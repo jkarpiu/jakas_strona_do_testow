73582c740a5e0d80ea3a66b0877e59e69640244f
kliberson <lucxsdesing14@gmail.com>
2020-12-08 00:12:35 +0100

    teściki

--- resources/js/components/TestsLists.vue
+++ resources/js/components/TestsLists.vue
@@ -1,11 +1,35 @@
 <template>
     <div>
-        <p :key="test.id" @click="$router.push('/test/' + test.id)" v-for="test in testy">{{ test.name }}</p>
-    </div>
+        <div>
+            <p :key="test.id" @click="$router.push('/test/' + test.id)" v-for="test in testy"></p>
+        </div>
+        <div class="wyniki_table">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Nazwa</th>
+                            <th>Dzial</th>
+                            <th>Termin</th>
+                            <th>Klasa</th>
+                        </tr>
+                    </thead>
+                    <tr :key="test.id" v-for="test in testy" @click="$router.push('/test/' + test.id)">
+                        <th>{{ test.name }}</th>
+                        <th>{{  }}</th>
+                        <th>{{ test.start }}</th>
+                       
+                        <th>{{ }}</th>
+                        
+                    </tr>
+                </table>
+            </div>
+    </div>        
 </template>
 <script>
 export default {
     props: ["testy"]
 };
 </script>
-<style scoped></style>
+<style scoped>
+
+</style>
